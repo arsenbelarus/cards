de61ea5247b3e83b0cbcb964c4d050e3d21bd76c
arsenbelarus <arsenbelarus@gmail.com>
2020-09-23 14:33:37 +0300

    Updates

---
+++ precache-manifest.d90e9cbb7f18e375c5b0a9992af94bcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a640c4c345d4ec1c5c8a6f8e00a0b2",
+    "url": "/cards/index.html"
+  },
+  {
+    "revision": "2c320e8d00389f9197e4",
+    "url": "/cards/static/css/main.9e95d8a3.chunk.css"
+  },
+  {
+    "revision": "9708834e851bc6606a76",
+    "url": "/cards/static/js/2.1986f399.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/cards/static/js/2.1986f399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c320e8d00389f9197e4",
+    "url": "/cards/static/js/main.5aeead49.chunk.js"
+  },
+  {
+    "revision": "2aa9159606923279774a",
+    "url": "/cards/static/js/runtime-main.06cc473a.js"
+  }
+]);
