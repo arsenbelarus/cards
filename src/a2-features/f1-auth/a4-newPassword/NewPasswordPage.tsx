90c9fe0d32b9e1d8c7e38d6dbed54b543a03917e
Andriy <refwol131@mail.ru>
2020-09-25 18:10:40 +0300

    Restore password page 🔥

--- src/a2-features/f1-auth/a4-newPassword/NewPasswordPage.tsx
+++ src/a2-features/f1-auth/a4-newPassword/NewPasswordPage.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import {setCorrectEmailAC} from "../../../a1-main/m2-bll/reducers/passwordRestore_reducer";
+import {useDispatch, useSelector} from "react-redux";
+import {AppRootStateType} from "../../../a1-main/m2-bll/store";
+import {Redirect} from "react-router-dom";
+import {RouterPathEnum} from "../../../a1-main/m1-ui/routes/routerPathsEnum";
 
 
 export const NewPasswordPage = () => {
-  return (
-    <div>
-      New Password page
-    </div>
-  )
+
+    const statusMessage = useSelector<AppRootStateType, string>(state => state.restorePassword.message)
+    const isCorrectEmail = useSelector<AppRootStateType, boolean>(state => state.restorePassword.isCorrectEmail)
+
+
+    const dispatch = useDispatch()
+    const logOutHandler = () => {
+        dispatch(setCorrectEmailAC(false))
+    }
+    return (
+        <div>
+            <div>{statusMessage}</div>
+            <button onClick={logOutHandler}>LogOut</button>
+        </div>
+    )
 }
